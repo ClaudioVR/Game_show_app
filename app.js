b5f9328a1a8186edd01e0bddac5ac1316bb8aa4f
Claudio <Claudio@Claudios-MacBook-Air.local>
2018-10-24 09:14:44 +0200

    First draft complete
    Everythign seems to work as hoped.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const startGameButton = document.querySelector('.btn__reset');
+const gameButton = document.querySelector('.btn__reset'); 
 const overlay = document.getElementById('overlay'); //welcome page
 
 const qwerty = document.getElementById('qwerty'); //keyboard buttons
 const phrase = document.getElementById('phrase'); //empty ul
-const button = document.querySelector('button');
+const button = document.querySelector('button'); 
 
-const show = document.getElementsByClassName('show');
-const letter = document.getElementsByClassName('letter');
-
+const show = document.getElementsByClassName('show'); // visible letters
+const letter = document.getElementsByClassName('letter'); //displayed but covered letters
+const liveHearts = document.querySelectorAll('.tries img');
 
 let missed = 0; //wrong answers
-let match = null;
+let match = null; //assume no correct answer to begin with
 
 const phrases = [
   'happy friday',
@@ -26,37 +26,37 @@ const phraseArray = getRandomPhraseAsArray(phrases);
 //hidden letters on display
 const visbilePhrase = document.querySelector('#phrase ul');
 
-
-
+// ---------------------------------------------------------------
 
 
 //remove overlay
 
-startGameButton.addEventListener('click', () => {
-    overlay.style.display = 'none';
-  });
+gameButton.addEventListener('click', () => {
+	overlay.style.display = 'none';
+});
+
 
 //get random phrase from array phrases
 
 function getRandomPhraseAsArray(arr) {
-  let randomPhrase = phrases[Math.floor(Math.random()*phrases.length)];
-  return randomPhrase.split("");
+	let randomPhrase = phrases[Math.floor(Math.random() * phrases.length)];
+	return randomPhrase.split("");
 }
 
 
 //display randomly picked phrase
 
 function addPhraseToDisplay(arr) {
-  for (i = 0; i < arr.length; i++) {
-    const listItem = document.createElement('li');
-    listItem.textContent = arr[i];
-    visbilePhrase.appendChild(listItem);
-    if (arr[i] !== " ") {
-      listItem.className = 'letter';
-    } else {
-      listItem.className = 'space';
-    }
-  }
+	for (i = 0; i < arr.length; i++) {
+		const listItem = document.createElement('li');
+		listItem.textContent = arr[i];
+		visbilePhrase.appendChild(listItem);
+		if (arr[i] !== " ") {
+			listItem.className = 'letter';
+		} else {
+			listItem.className = 'space';
+		}
+	}
 }
 
 addPhraseToDisplay(phraseArray); // call fuction to display phrase
@@ -67,55 +67,62 @@ addPhraseToDisplay(phraseArray); // call fuction to display phrase
 function checkLetter(guess) {
 	match = null;
 	let letters = document.querySelectorAll('.letter'); //unrevealed letters collection
-  for (i = 0; i < letters.length; i++) {
-    if (guess.textContent == letters[i].textContent) {
-      letters[i].classList.add('show');
+	for (i = 0; i < letters.length; i++) {
+		if (guess.textContent == letters[i].textContent) {
+			letters[i].classList.add('show');
 			match = true;
-    } 
-  } return match; 
+		}
+	}
+	return match;
 }
+
 
 //Event listener on keyboard letters
 
 qwerty.addEventListener('click', (e) => {
-  if (e.target.tagName == 'BUTTON') {
+	if (e.target.tagName == 'BUTTON') {
 		const chosenLetterButton = e.target;
-		chosenLetterButton.classList.add('chosen'); 
-		chosenLetterButton.disabled = 'true'; 
-    checkLetter(chosenLetterButton); 
-		
+		chosenLetterButton.classList.add('chosen');
+		chosenLetterButton.disabled = 'true';
+		checkLetter(chosenLetterButton);
+
 		const letterFound = match;
 		if (letterFound == null) {
-		missed++;
-		console.log('Failed guesses: ' + missed);
+			let i = missed;
+			liveHearts[i].setAttribute('src', 'images/lostHeart.png');
+			missed++;
 		}
-  }
-	
-	checkWin(); 
-	
+	}
+	checkWin();
 });
 
 
-// check if game is won or game over
+// check if game is won or game-over
 
 function checkWin() {
-
 	const header = document.querySelector('#overlay h2')
-	
+
 	if (show.length == letter.length) {
 		overlay.style.display = 'flex';
 		overlay.className = 'win';
 		header.textContent = 'Congratulations, you won.'
-		// TODO: change button to play again
+		gameButton.textContent = 'Play again'
+		reset();
+
 	} else if (missed >= 5) {
 		overlay.style.display = 'flex';
 		overlay.className = 'lose';
-		header.textContent = 'Game over.'
-		// TODO: change button to play again
-		}
+		header.textContent = 'Game over.';
+		gameButton.textContent = 'Play again';
+		reset();
+	}
 }
 
 
+// reload page function
 
-
-
+function reset() {
+	gameButton.addEventListener('click', () => {
+		location.reload();
+	});
+}
